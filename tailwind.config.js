c6edf57220f196870c959da75554fd9c8d97b449
Mahdi Tasha <imwhdiiii@gmail.com>
2023-07-05 21:22:41 +0330

    configs of tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// Codes By Mahdi Tasha
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{tsx}",
+    "./src/*.{tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
