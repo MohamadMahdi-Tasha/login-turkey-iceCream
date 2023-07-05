1e47b6b6912122d0102ecace026ee1005e2bcb97
Mahdi Tasha <imwhdiiii@gmail.com>
2023-07-05 22:11:21 +0330

    added small changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 // Importing Part
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css';
 
 // Variables
 const rootElement = document.getElementById('react-root') as HTMLElement;
@@ -10,5 +11,6 @@ const reactRootElement = ReactDOM.createRoot(rootElement);
 // Wrapping whole app inside Strict mode of React and rendering them in root element
 reactRootElement.render(
     <React.StrictMode>
+        <h1>sad</h1>
     </React.StrictMode>
 )
